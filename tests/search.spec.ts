3d2cfa01281f8ad6e7aee268806328a413e8d533
vyvuhoang <41622768+vyvuhoang@users.noreply.github.com>
2025-07-14 16:37:41 +0700

    Add test IDs, add more expect(), sync with spread sheet (#3)

--- tests/search.spec.ts
+++ tests/search.spec.ts
@@ -23,45 +23,50 @@ test.describe('Search Function Tests', () => {
         await login.login('Admin', 'admin123');
     });
 
-    test('Search "Admin" should return only "Admin"', async ({ page }) => {
+    test('[SEARCH_001] Search "Admin" should return only "Admin"', async ({ page }) => {
         const search = new SideBarSearch(page);
         await search.search('Admin');
         const items = await search.getVisibleMenuItems();
         expect(items).toEqual(['Admin']);
     });
 
-    test('Search "D" should return Admin, Dashboard, Directory', async ({ page }) => {
+    test('[SEARCH_002] Search "D" should return Admin, Dashboard, Directory', async ({ page }) => {
         const search = new SideBarSearch(page);
         await search.search('D');
         const items = await search.getVisibleMenuItems();
         expect(items.sort()).toEqual(['Admin', 'Dashboard', 'Directory'].sort());
     });
 
-    test('Search with no matching string shows empty list', async ({ page }) => {
+    test('[SEARCH_003] Search "aDmIn" should return only "Admin"', async ({ page }) => {
+        const search = new SideBarSearch(page);
+        await search.search('aDmIn');
+        const items = await search.getVisibleMenuItems();
+        expect(items).toEqual(['Admin']);
+    });
+
+    test('[SEARCH_004] Search with no matching string shows empty list', async ({ page }) => {
         const search = new SideBarSearch(page);
         await search.search('nothing');
         const items = await search.getVisibleMenuItems();
         expect(items).toEqual([]);
     });
 
-    for (const item of menuItems) {
-        test(`Searching "${item}" returns only "${item}"`, async ({ page }) => {
-            const search = new SideBarSearch(page);
-            await search.search(item);
-            const items = await search.getVisibleMenuItems();
-            expect(items).toEqual([item]);
-        });
-    }
-
-    test('Clearing the search input restores full menu', async ({ page }) => {
+    test('[SEARCH_005] Clearing the search input restores full menu', async ({ page }) => {
         const search = new SideBarSearch(page);
         await search.search('D');
         const threeItems = await search.getVisibleMenuItems();
         expect(threeItems.sort()).toEqual(['Admin', 'Dashboard', 'Directory'].sort());
         await search.search('');  // put empty string to clear search string
         const items = await search.getVisibleMenuItems();
-        expect(items).toContain('Admin');
-        expect(items).toContain('Dashboard');
-        expect(items).toContain('PIM');
+        expect(items.sort()).toEqual(menuItems.sort());
     });
+
+    for (const item of menuItems) {
+        test(`[SEARCH_006] Searching "${item}" returns only "${item}"`, async ({ page }) => {
+            const search = new SideBarSearch(page);
+            await search.search(item);
+            const items = await search.getVisibleMenuItems();
+            expect(items).toEqual([item]);
+        });
+    }
 });
